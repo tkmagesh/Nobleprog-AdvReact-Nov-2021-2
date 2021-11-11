63f2c461caab6ddf63690844b8b5d1cb882717d3
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-11 09:36:41 +0530

    updates

--- bug-tracker-app/src/bugs/components/BugEdit.jsx
+++ bug-tracker-app/src/bugs/components/BugEdit.jsx
@@ -6,6 +6,12 @@ const BugEdit = ({ addNew }) => {
         <section className="edit">
             <label htmlFor="">Bug Name :</label>
             <input type="text" onChange={evt => setNewBugName(evt.target.value)} />
+            <label> Project : </label>
+            <select>
+                <option value="">Select Project</option>
+                <option value="1">Project 1</option>
+                <option value="2">Project 2</option>
+            </select>
             <button onClick={() => addNew(newBugName)}>Add New</button>
         </section>
     )
